bccb37d93a5fbc09bbddedf895b436249570e65c
mxghj <dest-supp@proton.me>
2025-12-06 22:35:15 +0200

    Add font_name option in config file

--- stryde-docs/searcher-c2a407aa.js
+++ stryde-docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-19c65cef.js',
+                path_to_root + 'searchindex-3528733a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
