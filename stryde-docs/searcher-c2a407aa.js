b54aaeba62c7fd9b9c7c4dcfce0dc78c839281a9
mxghj <dest-supp@proton.me>
2025-12-07 17:26:27 +0200

    Add new options in config file

--- stryde-docs/searcher-c2a407aa.js
+++ stryde-docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3528733a.js',
+                path_to_root + 'searchindex-402d4a05.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
